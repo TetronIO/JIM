235403edf02579b22b389ae4ca756ab537aa9f1d
Jay Van der Zant <amethi@gmail.com>
2022-02-20 21:14:16 +0000

    Tidyup

--- src/JIM.Models/Core/MetaverseObject.cs
+++ src/JIM.Models/Core/MetaverseObject.cs
@@ -9,17 +9,10 @@ namespace JIM.Models.Core
         public DateTime? LastUpdated { get; set; }
         public MetaverseObjectType Type { get; set; }
         public List<MetaverseObjectAttributeValue> AttributeValues { get; set; }
-        /// <summary>
-        /// Metaverse objects of type User can have roles.
-        /// In the future, more object types may be able to have roles assigned to them.
-        /// Roles grant entitlements within JIM.
-        /// </summary>
-        //public List<Role> Roles { get; set; }
 
         public MetaverseObject()
         {
             AttributeValues = new List<MetaverseObjectAttributeValue>();
-            //Roles = new List<Role>();
         }
     }
 }
