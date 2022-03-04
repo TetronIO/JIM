108438e303fdd0b84183f1e27972ada56c6594b1
Jay Van der Zant <amethi@gmail.com>
2022-03-04 09:31:12 +0000

    New API endpoint for roles

---
+++ src/JIM.Api/Controllers/SecurityController.cs
@@ -0,0 +1,27 @@
+﻿using JIM.Application;
+using JIM.Models.Security;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JIM.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly ILogger<SynchronisationController> _logger;
+        private readonly JimApplication _application;
+
+        public SecurityController(ILogger<SynchronisationController> logger, JimApplication application)
+        {
+            _logger = logger;
+            _application = application;
+        }
+
+        [HttpGet("/security/roles")]
+        public IEnumerable<Role> GetRoles()
+        {
+            _logger.LogTrace($"Someone requested the roles");
+            return _application.Security.GetRoles();
+        }
+    }
+}
