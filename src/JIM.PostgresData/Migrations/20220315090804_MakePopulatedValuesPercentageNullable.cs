85f1b7960ac68e4e02caf0a2689c4dbe3df11c7d
Jay Van der Zant <amethi@gmail.com>
2022-03-21 08:51:05 +0000

    Adding support for manager depth in data gen

---
+++ src/JIM.PostgresData/Migrations/20220315090804_MakePopulatedValuesPercentageNullable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JIM.PostgresData.Migrations
+{
+    public partial class MakePopulatedValuesPercentageNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PopulatedValuesPercentage",
+                table: "DataGenerationTemplateAttributes",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerDepthPercentage",
+                table: "DataGenerationTemplateAttributes",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ManagerDepthPercentage",
+                table: "DataGenerationTemplateAttributes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PopulatedValuesPercentage",
+                table: "DataGenerationTemplateAttributes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
