c0fac2323d41860564162a4158fe52d4bd69540a
Jay Van der Zant <amethi@gmail.com>
2022-03-29 15:14:25 +0100

    db migration for data gen min/max mva refs

---
+++ src/JIM.PostgresData/Migrations/20220329141331_AddDataGenMinMaxMvaRefControls.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JIM.PostgresData.Migrations
+{
+    public partial class AddDataGenMinMaxMvaRefControls : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MvaRefMaxAssignments",
+                table: "DataGenerationTemplateAttributes",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MvaRefMinAssignments",
+                table: "DataGenerationTemplateAttributes",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MvaRefMaxAssignments",
+                table: "DataGenerationTemplateAttributes");
+
+            migrationBuilder.DropColumn(
+                name: "MvaRefMinAssignments",
+                table: "DataGenerationTemplateAttributes");
+        }
+    }
+}
