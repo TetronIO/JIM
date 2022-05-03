c6edcf5f0aa1027a98b36ee920ad02a4f5c9fc6e
Jay Van der Zant <amethi@gmail.com>
2022-05-03 09:10:32 +0100

    OIDC Attempt

--- src/JIM.Web/Program.cs
+++ src/JIM.Web/Program.cs
@@ -1,4 +1,33 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// setup OpenID Connect (OIDC) Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddCookie(setup => setup.ExpireTimeSpan = TimeSpan.FromMinutes(60))
+    .AddOpenIdConnect(options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        //options.Authority = builder.Configuration["Auth.Authority"];
+        options.MetadataAddress = builder.Configuration["Auth.MetadataUrl"];
+        options.ClientId = builder.Configuration["Auth.ClientId"];
+        options.ClientSecret = builder.Configuration["Auth.ClientSecret"];
+        options.ResponseType = "code id_token";
+        options.SaveTokens = true;
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -15,12 +44,10 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
