ddb4c44c73e29099a5964df7f35dcf5c734f71b5
Jay Van der Zant <amethi@gmail.com>
2022-05-07 16:09:20 +0100

    Basic user bar added

--- src/JIM.Web/Program.cs
+++ src/JIM.Web/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Identity.Web.UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // setup OpenID Connect (OIDC) authentication
@@ -21,6 +23,7 @@ builder.Services.AddAuthentication(options =>
         options.Scope.Add("openid");
         options.Scope.Add("profile");
     });
+builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
 // setup authorisation policies
 builder.Services.AddAuthorization(options =>
