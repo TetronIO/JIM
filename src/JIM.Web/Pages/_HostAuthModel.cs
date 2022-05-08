b5c7beaed1e2007bbc348d3ef5b59cec7bed86bf
Jay Van der Zant <amethi@gmail.com>
2022-05-08 10:32:01 +0100

    Sign-out now working

---
+++ src/JIM.Web/Pages/_HostAuthModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JIM.Web.Pages
+{
+    public class _HostAuthModel : PageModel
+    {
+        public async Task OnGetSignout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc", AuthProps());
+        }
+
+        private AuthenticationProperties AuthProps() => new AuthenticationProperties
+        {
+            RedirectUri = Url.Content("~/")
+        };
+    }
+}
