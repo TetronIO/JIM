20d0d2e10cb014c5edcfd033dd6b61d761c1a890
Amethi <amethi@gmail.com>
2022-12-23 17:00:59 +0000

    Adding Partition to Run Profiles

--- src/JIM.Models/Staging/ConnectedSystemRunProfile.cs
+++ src/JIM.Models/Staging/ConnectedSystemRunProfile.cs
@@ -5,7 +5,14 @@ namespace JIM.Models.Staging
     public class ConnectedSystemRunProfile
     {
         public int Id { get; set; }
+        
         public ConnectedSystem ConnectedSystem { get; set; }
+        
+        /// <summary>
+        /// If the connected system implements partitions, then a run profile needs to target a partition.
+        /// </summary>
+        public ConnectedSystemPartition? Partition { get; set; }
+
         public SyncRunType RunType { get; set; }
     }
 }
