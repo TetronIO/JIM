3b04313c8fd1d7852a2bde42a4ca37de973a3737
Jay Van der Zant <amethi@gmail.com>
2022-12-27 10:45:36 +0000

    Read view for connectors

--- src/JIM.Connectors/Constants.cs
+++ src/JIM.Connectors/Constants.cs
@@ -2,6 +2,9 @@
 {
     public static class Constants
     {
+        // these connector name constants are used to help identify built-in connectors that haven't been updated from NuGet packages.
+        // this enables a JIM instance to function that hasn't, or isn't able to be (disconnected environments) updated connectors from NuGet packages.
+
         public static string LdapConnectorName => "JIM LDAP Connector";
         public static string CsvConnectorName = "JIM CSV Connector";
         public static string Scim2ConnectorName = "JIM SCIM2 Connector";
