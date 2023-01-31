e5f010d6375088cc4b5fcd4dc071178413d94412
Amethi <amethi@gmail.com>
2023-01-31 20:34:56 +0000

    NuGet updates, WIP

--- reset-jim.ps1
+++ reset-jim.ps1
@@ -2,3 +2,6 @@ docker kill $(docker ps -q)
 docker system prune -f
 docker volume rm jim-db-volume
 docker volume rm jim-logs-volume
+
+# delete all bin and obj folders
+gci -include bin,obj -recurse | remove-item -force -recurse
