1bfd1af7f11c615b01fa2ee17d8c1740d7b7b7a1
Amethi <amethi@gmail.com>
2023-02-26 21:03:34 +0000

    UI migration to MudBlazor

--- src/JIM.Web/Models/ConnectedSystemCreateViewModel.cs
+++ src/JIM.Web/Models/ConnectedSystemCreateViewModel.cs
@@ -4,12 +4,10 @@ namespace JIM.Web.Models
 {
     public class ConnectedSystemCreateViewModel
     {
-        [Required(ErrorMessage = "Please select a connector")]
         public string ConnectorId { get; set; }
 
-        [Required(ErrorMessage = "Please provide a name for the new connected system")]
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
