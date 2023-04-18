ab828e33d71596178d79e630425e6a8a6890a5a6
Amethi <amethi@gmail.com>
2023-04-18 09:06:21 +0100

    Adding service task model support for concurrent tasks

--- src/JIM.Models/Tasking/Enums.cs
+++ src/JIM.Models/Tasking/Enums.cs
@@ -4,5 +4,16 @@
 	{
 		Queued = 0,
 		Processing = 1,
+		CancellationRequested = 2
+	}
+
+	/// <summary>
+	/// Determines whether a task must be executed on it's own, i.e. sequentially, 
+	/// or if it can be run in parallel with other tasks.
+	/// </summary>
+	public enum ServiceTaskExecutionMode
+	{
+		Sequential = 0,
+		Parallel = 1
 	}
 }
--- src/JIM.Models/Tasking/ServiceTask.cs
+++ src/JIM.Models/Tasking/ServiceTask.cs
@@ -3,13 +3,22 @@
 	public abstract class ServiceTask
 	{
 		public Guid Id { get; set; }
+
+		/// <summary>
+		/// Typically the value for the timestamp will be when the task was created, though the value can
+		/// be changed to change the order in which the tasks will be processed in relation to others, i.e. it controls ordering.
+		/// </summary>
 		public DateTime Timestamp { get; set; }
+
 		public ServiceTaskStatus Status { get; set; }
 
-		public ServiceTask()
+		public ServiceTaskExecutionMode ExecutionMode { get; set; }
+
+        public ServiceTask()
         {
 			Timestamp = DateTime.Now;
 			Status = ServiceTaskStatus.Queued;
+			ExecutionMode = ServiceTaskExecutionMode.Sequential;
         }
 	}
 }
