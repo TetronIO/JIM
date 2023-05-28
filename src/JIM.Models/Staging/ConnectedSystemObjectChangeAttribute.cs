68b34f2700336b00a14fbd7d29e38ee23df2bc5a
Jay Van der Zant <amethi@gmail.com>
2023-05-28 10:14:31 +0100

    Remodelling CSO attribute value changes

---
+++ src/JIM.Models/Staging/ConnectedSystemObjectChangeAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace JIM.Models.Staging
+{
+    public class ConnectedSystemObjectChangeAttribute
+    {
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// The parent for this object.
+        /// Required for establishing an Entity Framework relationship.
+        /// </summary>
+        public ConnectedSystemObjectChange ConnectedSystemChange { get; set; }
+
+        /// <summary>
+        /// The connected system attribute these value changes relates to.
+        /// </summary>
+        public ConnectedSystemAttribute ConnectedSystemAttribute { get; set; }
+
+        /// <summary>
+        /// A list of what values were added to this attribute.
+        /// </summary>
+        public List<ConnectedSystemObjectChangeAttributeItemValue> ValuesAdded { get; set; } = new List<ConnectedSystemObjectChangeAttributeItemValue>();
+
+        /// <summary>
+        /// A list of what values were renmoved from this attribute.
+        /// </summary>
+        public List<ConnectedSystemObjectChangeAttributeItemValue> ValuesRemoved { get; set; } = new List<ConnectedSystemObjectChangeAttributeItemValue>();
+    }
+}
