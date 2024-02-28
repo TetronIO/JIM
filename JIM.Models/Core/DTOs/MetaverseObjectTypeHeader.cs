8cbd53d06b8e4d9daceea90804dc0f544539a418
Jay Van der Zant <amethi@gmail.com>
2024-02-28 23:45:00 +0000

    Minor edits

--- JIM.Models/Core/DTOs/MetaverseObjectTypeHeader.cs
+++ JIM.Models/Core/DTOs/MetaverseObjectTypeHeader.cs
@@ -3,7 +3,7 @@
     public class MetaverseObjectTypeHeader
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = null!;
         public DateTime Created { get; set; }
         public int AttributesCount { get; set; }
         public bool BuiltIn { get; set; }
