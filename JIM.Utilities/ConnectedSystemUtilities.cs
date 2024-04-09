aeef4ef88998f73b501dada01903c56eb0ca45ed
Jay Van der Zant <amethi@gmail.com>
2024-04-09 08:05:39 +0100

    JIM.Utilities Rider improvements

--- JIM.Utilities/ConnectedSystemUtilities.cs
+++ JIM.Utilities/ConnectedSystemUtilities.cs
@@ -1,44 +1,42 @@
 ﻿using JIM.Models.Staging;
+namespace JIM.Utilities;
 
-namespace JIM.Utilities
+public static class ConnectedSystemUtilities
 {
-    public static class ConnectedSystemUtilities
+    /// <summary>
+    /// Generates a list of all selected containers in a partition container hierarchy. Uses recursion to walk the hierarchy.
+    /// </summary>
+    public static List<ConnectedSystemContainer> GetAllSelectedContainers(ConnectedSystemPartition connectedSystemPartition)
     {
-        /// <summary>
-        /// Generates a list of all selected containers in a partition container hierarchy. Uses recursion to walk the hierarchy.
-        /// </summary>
-        public static List<ConnectedSystemContainer> GetAllSelectedContainers(ConnectedSystemPartition connectedSystemPartition)
+        if (connectedSystemPartition == null)
+            throw new ArgumentNullException(nameof(connectedSystemPartition));
+
+        if (connectedSystemPartition.Containers == null)
+            throw new ArgumentException("ConnectedSystemContainer.Containers is null", nameof(connectedSystemPartition.Containers));
+
+        var selectedContainers = new List<ConnectedSystemContainer>();
+        foreach (var rootContainer in connectedSystemPartition.Containers)
         {
-            if (connectedSystemPartition == null)
-                throw new ArgumentNullException(nameof(connectedSystemPartition));
+            if (rootContainer.Selected)
+                selectedContainers.Add(rootContainer);
 
-            if (connectedSystemPartition.Containers == null)
-                throw new ArgumentException("ConnectedSystemContainer.Containers is null", nameof(connectedSystemPartition.Containers));
-
-            var selectedContainers = new List<ConnectedSystemContainer>();
-            foreach (var rootContainer in connectedSystemPartition.Containers)
-            {
-                if (rootContainer.Selected)
-                    selectedContainers.Add(rootContainer);
-
-                SearchForSelectedChildContainers(rootContainer, selectedContainers);
-            }
-
-            return selectedContainers;
+            SearchForSelectedChildContainers(rootContainer, selectedContainers);
         }
 
-        private static void SearchForSelectedChildContainers(ConnectedSystemContainer container, List<ConnectedSystemContainer> selectedContainers)
+        return selectedContainers;
+    }
+
+    private static void SearchForSelectedChildContainers(ConnectedSystemContainer container, ICollection<ConnectedSystemContainer> selectedContainers)
+    {
+        if (container.ChildContainers.Count == 0)
+            return;
+
+        foreach (var childContainer in container.ChildContainers)
         {
-            if (container == null || container.ChildContainers == null || container.ChildContainers.Count == 0)
-                return;
+            if (childContainer.Selected)
+                selectedContainers.Add(childContainer);
 
-            foreach (var childContainer in container.ChildContainers)
-            {
-                if (childContainer.Selected)
-                    selectedContainers.Add(childContainer);
-
-                SearchForSelectedChildContainers(childContainer, selectedContainers);
-            }
+            SearchForSelectedChildContainers(childContainer, selectedContainers);
         }
     }
 }
--- JIM.Utilities/GeneralUtilities.cs
+++ JIM.Utilities/GeneralUtilities.cs
@@ -3,79 +3,74 @@ using JIM.Models.Core.DTOs;
 using JIM.Models.Staging;
 using JIM.Models.Staging.DTOs;
 using System.Text.RegularExpressions;
+namespace JIM.Utilities;
 
-namespace JIM.Utilities
+public static class Utilities
 {
-    public static class Utilities
+    public static string SplitOnCapitalLetters(this string inputString)
     {
-        public static string SplitOnCapitalLetters(this string inputString)
-        {
-            var words = Regex.Matches(inputString, @"([A-Z][a-z]+)").Cast<Match>().Select(m => m.Value);
-            var withSpaces = string.Join(" ", words);
-            return withSpaces;
-        }
+        var words = Regex.Matches(inputString, @"([A-Z][a-z]+)").Cast<Match>().Select(m => m.Value);
+        var withSpaces = string.Join(" ", words);
+        return withSpaces;
+    }
         
-        public static bool AreByteArraysTheSame(ReadOnlySpan<byte> array1, ReadOnlySpan<byte> array2)
-        {
-            // byte[] is implicitly convertible to ReadOnlySpan<byte>
-            return array1.SequenceEqual(array2);
-        }
+    public static bool AreByteArraysTheSame(ReadOnlySpan<byte> array1, ReadOnlySpan<byte> array2)
+    {
+        // byte[] is implicitly convertible to ReadOnlySpan<byte>
+        return array1.SequenceEqual(array2);
+    }
 
-        public static string GetMetaverseObjectHref(MetaverseObject metaverseObject)
-        {
-            if (metaverseObject == null)
-                return string.Empty;
+    public static string GetMetaverseObjectHref(MetaverseObject metaverseObject)
+    {
+        return $"/t/{metaverseObject.Type.Name.ToLower()}/v/{metaverseObject.Id}";
+    }
+    public static string? GetMetaverseObjectHrefText(MetaverseObject metaverseObject)
+    {
+        return metaverseObject.GetAttributeValue(Constants.BuiltInAttributes.DisplayName)?.StringValue;
+    }
 
-            return $"/t/{metaverseObject.Type.Name.ToLower()}/v/{metaverseObject.Id}";
-        }
-        public static string? GetMetaverseObjectHrefText(MetaverseObject metaverseObject)
-        {
-            return metaverseObject.GetAttributeValue(Constants.BuiltInAttributes.DisplayName)?.StringValue;
-        }
+    public static string GetMetaverseObjectHref(MetaverseObjectHeader metaverseObjectHeader)
+    {
+        return $"/t/{metaverseObjectHeader.TypeName.ToLower()}/v/{metaverseObjectHeader.Id}";
+    }
 
-        public static string GetMetaverseObjectHref(MetaverseObjectHeader metaverseObjectHeader)
-        {
-            return $"/t/{metaverseObjectHeader.TypeName.ToLower()}/v/{metaverseObjectHeader.Id}";
-        }
+    public static string? GetMetaverseObjectHrefText(MetaverseObjectHeader metaverseObjectHeader)
+    {
+        return metaverseObjectHeader.GetAttributeValue(Constants.BuiltInAttributes.DisplayName)?.StringValue;
+    }
 
-        public static string? GetMetaverseObjectHrefText(MetaverseObjectHeader metaverseObjectHeader)
-        {
-            return metaverseObjectHeader.GetAttributeValue(Constants.BuiltInAttributes.DisplayName)?.StringValue;
-        }
+    public static string GetConnectedSystemHref(ConnectedSystem connectedSystem)
+    {
+        return $"/admin/connected-systems/{connectedSystem.Id}";
+    }
 
-        public static string GetConnectedSystemHref(ConnectedSystem connectedSystem)
-        {
-            return $"/admin/connected-systems/{connectedSystem.Id}";
-        }
+    public static string GetConnectedSystemHref(ConnectedSystemHeader connectedSystemHeader)
+    {
+        return $"/admin/connected-systems/{connectedSystemHeader.Id}";
+    }
 
-        public static string GetConnectedSystemHref(ConnectedSystemHeader connectedSystemHeader)
-        {
-            return $"/admin/connected-systems/{connectedSystemHeader.Id}";
-        }
+    public static string GetConnectedSystemObjectsHref(ConnectedSystemHeader connectedSystemHeader)
+    {
+        return GetConnectedSystemObjectsHref(connectedSystemHeader.Id);
+    }
 
-        public static string GetConnectedSystemObjectsHref(ConnectedSystemHeader connectedSystemHeader)
-        {
-            return GetConnectedSystemObjectsHref(connectedSystemHeader.Id);
-        }
+    public static string GetConnectedSystemObjectsHref(int connectedSystemId)
+    {
+        return $"/admin/connected-systems/{connectedSystemId}/objects";
+    }
 
-        public static string GetConnectedSystemObjectsHref(int connectedSystemId)
-        {
-            return $"/admin/connected-systems/{connectedSystemId}/objects";
-        }
+    public static string GetConnectedSystemObjectHref(ConnectedSystemObject connectedSystemObject)
+    {
+        return GetConnectedSystemObjectHref(connectedSystemObject.ConnectedSystemId, connectedSystemObject.Id);
+    }
 
-        public static string GetConnectedSystemObjectHref(ConnectedSystemObject connectedSystemObject)
-        {
-            return GetConnectedSystemObjectHref(connectedSystemObject.ConnectedSystemId, connectedSystemObject.Id);
-        }
+    public static string GetConnectedSystemObjectHref(ConnectedSystemObjectHeader connectedSystemObjectHeader)
+    {
+        return GetConnectedSystemObjectHref(connectedSystemObjectHeader.ConnectedSystemId, connectedSystemObjectHeader.Id);
+    }
 
-        public static string GetConnectedSystemObjectHref(ConnectedSystemObjectHeader connectedSystemObjectHeader)
-        {
-            return GetConnectedSystemObjectHref(connectedSystemObjectHeader.ConnectedSystemId, connectedSystemObjectHeader.Id);
-        }
-
-        public static string GetConnectedSystemObjectHref(int connectedSystemId, Guid connectedSystemObjectId)
-        {
-            return $"/admin/connected-systems/{connectedSystemId}/objects/{connectedSystemObjectId}";
-        }
+    public static string GetConnectedSystemObjectHref(int connectedSystemId, Guid connectedSystemObjectId)
+    {
+        return $"/admin/connected-systems/{connectedSystemId}/objects/{connectedSystemObjectId}";
     }
 }
