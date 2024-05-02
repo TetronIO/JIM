dfb0b4e9a5a390908a71b9022157808ba1415cfc
Jay Van der Zant <amethi@gmail.com>
2024-05-02 09:17:11 +0100

    FIX: unintended attrib change on import ref update test

--- JIM.Worker.Tests/Synchronisation/ImportUpdateObjectTests.cs
+++ JIM.Worker.Tests/Synchronisation/ImportUpdateObjectTests.cs
@@ -1251,7 +1251,7 @@ public class ImportUpdateObjectTests
                 new()
                 {
                     Name = MockAttributeName.LEAVER.ToString(),
-                    BoolValue = true,
+                    BoolValue = false,
                     Type = AttributeDataType.Boolean
                 }
             }
